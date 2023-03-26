64404413f71371fe75016da277d0742959569ab6
Tyson <deppravacion@gmail.com>
2023-03-26 14:57:47 -0600

    taskify functional

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Todo } from "../model";
 import { AiFillEdit, AiFillDelete} from 'react-icons/ai'
 import { MdDone } from 'react-icons/md'
@@ -33,13 +33,23 @@ const SingleTodo = ({todo, todos, setTodos}: Props) => {
     setTodos(todos.map(
       (todo) => (todo.id === id ? { ...todo, todo: editTodo} : todo)      
     ))
-    setEdit(false)
+    setEdit(false)   
   }
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [edit])
+
+
+
   return (
     <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
 
     {edit ? (
-      <input 
+      <input
+        ref={inputRef} 
         value={editTodo} 
         onChange={(e) => setEditTodo(e.target.value)}
         className='todos__single--text'
--- src/model.ts
+++ src/model.ts
@@ -1,5 +1,38 @@
+
 export interface Todo {
   id: number;
   todo: string;
   isDone: boolean;
 }
+
+// useReducer @ 1:04:00
+
+// import { useReducer } from "react";
+
+// type Actions =
+//  | { type: 'add', payload: string }
+//  | { type: 'remove', payload: number }
+//  | { type: 'done', payload: number }
+
+
+// const TodoReducer = (state: Todo[], action:Actions) => {
+//   switch (action.type) {
+//     case 'add':
+//       return [
+//         ...state, 
+//         { id: Date.now(), todo: action.payload, isDone: false}
+//       ]
+//     case 'remove': 
+//       return state.filter((todo) => todo.id !== action.payload) 
+//     case 'done': 
+//       return state.map((todo) => {
+//         todo.id !== action.payload ? { ...todo, idDone: !todo.isDone} : todo }
+//       )
+//     default: 
+//         return state
+//   }
+// }
+
+// const reducerExample = () => {
+//   const [state, dispatch] = useReducer(TodoReducer, [])
+// }
